49cada5c48bf3d12429588577df6ba33019761f4
Nathaniel Landau <nate@natelandau.com>
2024-01-06 15:24:44 -0500

    feat(test): add debug fixture to conftest

---
+++ {{ cookiecutter.__package_name_kebab_case }}/tests/conftest.py
@@ -0,0 +1,38 @@
+# type: ignore
+"""Shared fixtures for tests."""
+
+from pathlib import Path
+
+import pytest
+
+from {{ cookiecutter.__package_name_snake_case }}.utils import console
+
+def debug():
+    """Print debug information to the console. This is used to debug tests while writing them."""
+
+    def _debug_inner(label: str, value: str | Path, breakpoint: bool = False):
+        """Print debug information to the console. This is used to debug tests while writing them.
+
+        Args:
+            label (str): The label to print above the debug information.
+            value (str | Path): The value to print. When this is a path, prints all files in the path.
+            breakpoint (bool, optional): Whether to break after printing. Defaults to False.
+
+        Returns:
+            bool: Whether to break after printing.
+        """
+        console.rule(label)
+        if not isinstance(value, Path) or not value.is_dir():
+            console.print(value)
+        else:
+            for p in value.rglob("*"):
+                console.print(p)
+
+        console.rule()
+
+        if breakpoint:
+            return pytest.fail("Breakpoint")
+
+        return True
+
+    return _debug_inner
