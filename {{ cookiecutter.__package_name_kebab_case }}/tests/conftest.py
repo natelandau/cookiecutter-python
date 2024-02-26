c5a4ee0e9b05a5ab2ef77df6289b2423313626e6
Nathaniel Landau <natelandau@users.noreply.github.com>
2024-02-26 10:53:03 -0500

    fix(logging): add DRYRUN log level (#56)

--- {{ cookiecutter.__package_name_kebab_case }}/tests/conftest.py
+++ {{ cookiecutter.__package_name_kebab_case }}/tests/conftest.py
@@ -13,23 +13,26 @@ from {{ cookiecutter.__package_name_snake_case }}.utils import console, {{ cooki
 def debug():
     """Print debug information to the console. This is used to debug tests while writing them."""
 
-    def _debug_inner(label: str, value: str | Path, breakpoint: bool = False):
+    def _debug_inner(label: str, value: str | Path, breakpoint: bool = False, width: int = 80):
         """Print debug information to the console. This is used to debug tests while writing them.
 
         Args:
             label (str): The label to print above the debug information.
             value (str | Path): The value to print. When this is a path, prints all files in the path.
             breakpoint (bool, optional): Whether to break after printing. Defaults to False.
+            width (int, optional): The width of the console output. Defaults to 80, pytest's default when running without `-s`.
 
         Returns:
             bool: Whether to break after printing.
         """
         console.rule(label)
-        if not isinstance(value, Path) or not value.is_dir():
-            console.print(value)
-        else:
+
+        # If a directory is passed, print the contents
+        if isinstance(value, Path) and value.is_dir():
             for p in value.rglob("*"):
-                console.print(p)
+                console.print(p, width=width)
+        else:
+            console.print(value, width=width)
 
         console.rule()
 
@@ -42,40 +45,37 @@ def debug():
 
 
 @pytest.fixture()
-def config_data():
-    """Mock specific configuration data for use in tests."""
+def mock_config():
+    """Mock specific configuration data for use in tests by accepting arbitrary keyword arguments.
 
-    def _inner(
-        key_one: str | None = None,
-        key_two: list[int] | None = None,
-        key_three: tuple[str, ...] | None = None,
-    ):
-        override_data = {}
-        if key_one:
-            override_data["key_one"] = key_one
-        if key_two:
-            override_data["key_two"] = key_two
-        if key_three:
-            override_data["key_three"] = key_three
+    The function dynamically collects provided keyword arguments, filters out any that are None,
+    and prepares data sources with the overridden configuration for file processing.
 
-        mock_config_file = Path(__file__).resolve().parent / "fixtures/default_config.toml"
+    Usage:
+        def test_something(mock_config):
+            # Override the configuration with specific values
+            with {{ cookiecutter.__package_name_pascal_case }}Config.change_config_sources(config_data(some_key="some_value")):
+                    # Test the functionality
+                    result = do_something()
+                    assert result
+    """
 
-        return [
-            FileSource(mock_config_file),
-            DataSource(data=override_data),
-        ]
+    def _inner(**kwargs):
+        """Collects provided keyword arguments, omitting any that are None, and prepares data sources with the overridden configuration.
+
+        Args:
+            **kwargs: Arbitrary keyword arguments representing configuration settings.
+
+        Returns:
+            list: A list containing a FileSource initialized with the fixture configuration and a DataSource with the overridden data.
+        """
+        # Filter out None values from kwargs
+        override_data = {key: value for key, value in kwargs.items() if value is not None}
+
+        # Define the path to the fixture configuration file
+        fixture_config = Path(__file__).resolve().parent / "fixtures/fixture_config.toml"
+
+        # Return a list of data sources with the overridden configuration
+        return [FileSource(fixture_config), DataSource(data=override_data)]
 
     return _inner
-
-
-@pytest.fixture()
-def mock_config():  # noqa: PT004
-    """Override configuration file with mock configuration for use in tests. To override a default use the `config_data` fixture.
-
-    Returns:
-        {{ cookiecutter.__package_name_pascal_case }}Config: The mock configuration.
-    """
-    mock_config_file = Path(__file__).resolve().parent / "fixtures/default_config.toml"
-
-    with {{ cookiecutter.__package_name_pascal_case }}Config.change_config_sources(FileSource(mock_config_file)):
-        yield
